f743a09b15a3e2dd6e6b6148c6766e234abc9527
sadik312 <sadik_islam123@icloud.com>
2021-01-27 00:19:19 +0000

    started editing app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById('r');
+const paperdiv = document.getElementById('p');
+const scissors_div = document.getElementById('s');
+
+function getComputerChoice() {
+  const choices['r', 'p', 's'];
+  console.log(Math.random());
+}
+
+function game(userChoice) {
+  console.log("asdf" + userChoice);
+}
+
+function main() {
+  rock_div.addEventListener("click", function() {
+    game("r");
+  })
+
+  paper_div.addEventListener("click", function() {
+    game("p");
+  })
+
+  scissors_div.addEventListener("click", function() {
+    game("s");
+  })
+}
+
+main();
+
+video time: 40mins
